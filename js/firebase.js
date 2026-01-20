e41872fb481f367a1236e999249f78d27084f062
TheMinescout <tmcarleton11@gmail.com>
2026-01-20 10:41:33 -0800

    Create firebase.js

---
+++ js/firebase.js
@@ -0,0 +1,11 @@
+// js/firebase-config.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyAZE1qCY8YCZLrZ2EWT0QTFRoBVHvabqDM",
+  authDomain: "minescout-life-4a668.firebaseapp.com",
+  databaseURL: "https://minescout-life-4a668-default-rtdb.firebaseio.com",
+  projectId: "minescout-life-4a668",
+  storageBucket: "minescout-life-4a668.firebasestorage.app",
+  messagingSenderId: "351393417322",
+  appId: "1:351393417322:web:92c9a92a50e60890136213",
+  measurementId: "G-RFP78TP9KZ"
+};
